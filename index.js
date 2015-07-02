c275bcf837558c4996b118c590f180a6e1f189a5
amitmtrn <amit@mtrn.co.il>
2015-07-03 02:04:19 +0300

    init

---
+++ index.js
@@ -0,0 +1,42 @@
+var nodeLib = 'fs';
+var lib = require(nodeLib);
+
+for (var key in lib) {
+  if (key.indexOf('Sync') === -1 &&
+      lib[key].toString().indexOf('callback') > -1) {
+
+    var paramsName = getParamNames(lib[key]);
+
+    for (var i = 0; i <= paramsName.length; i++) {
+      if (paramsName[i].indexOf('callback') > -1) {
+        break;
+      }
+    }
+
+    (function(key, asyncFunc, callbackIndex) {
+      var newKey = key + 'Promise';
+      asyncFunc[newKey] = function() {
+        console.log(callbackIndex);
+        asyncFunc[key].apply(this, arguments);
+      };
+    }(key, lib, i));
+
+  }
+}
+/**
+ *
+ * http://stackoverflow.com/questions/1007981/how-to-get-function-parameter-names-values-dynamically-from-javascript
+ */
+function getParamNames(func) {
+  var STRIP_COMMENTS = /((\/\/.*$)|(\/\*[\s\S]*?\*\/))/mg;
+  var ARGUMENT_NAMES = /([^\s,]+)/g;
+  var fnStr = func.toString().replace(STRIP_COMMENTS, '');
+  var result = fnStr.slice(fnStr.indexOf('(') + 1, fnStr.indexOf(')')).match(ARGUMENT_NAMES);
+  if (result === null)
+     result = [];
+  return result;
+}
+
+lib.existsPromise(__filename, function(exists) {
+  console.log(exists);
+});
